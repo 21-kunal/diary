2a18d21e51ce35eaba239163c4c37ec2b5d1283f
Kunal Prajapati <kunalprajapati544@gmail.com>
2024-02-18 02:45:24 +0530

    adding database file

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from database import add_entry,get_entries
+
+
 menu = """Select one the following options:
 1) Record your thoughts in the diary.
 2) Take a look at the diary.
@@ -8,12 +11,24 @@ Your Selection:
 
 welcome = "Welcome to the diary!"
 
+def new_entry():
+    entry_content = input("What is on your mind? ")
+    entry_date = input("Enter the Date: ") 
+    add_entry(entry_content,entry_date)
+
+
+def view_entries(entires):
+    for entry in entires:
+        print(f"Date: {entry['date']}\n{entry['content']}\n\n")
+
+
 print(welcome)
 
 while (user_input := input(menu)) != "3":
     if user_input == "1":
-        print("Writing....")
+        new_entry()
     elif user_input == "2":
-        print("Looking...")
+        entires = get_entries()
+        view_entries(entires)
     else:
         print("Invalid option, please try again!")
